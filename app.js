224d8ee61fa7c9e68f5799850f0079a609c8d575
Nur Mohammed Imtiaz <76715694+fimtiaz4@users.noreply.github.com>
2022-06-05 01:39:51 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+const book1 = {
+    name: "Book11",
+    writer: "writer2",
+    price: 0,
+    rating: 4.5
+}
+const book2 = {
+    name: "Book22",
+    writer: "writer2",
+    price: 00,
+    rating: 4.5
+}
+const book3 = {
+    name: "Book22",
+    writer: "writer2",
+    price: 540,
+    rating: 4.5
+}
+
+const cart = [book1 , book2 , book3]
+let bookPrice = 0;
+const shipping = 50;
+
+cart.forEach(function(product){
+        bookPrice += product.price
+    })
+
+let discounts = bookPrice*0.05
+let discount = Math.ceil(discounts);
+let discountedPrice = bookPrice - discount
+
+if(bookPrice>=500){
+
+    if(bookPrice>2400){
+        discount = 120
+        discountedPrice = bookPrice - discount
+    }
+
+}else{
+    discount = 0
+    discountedPrice = bookPrice
+}
+
+console.log(`Shopping Amount        - ${bookPrice} Taka,`)
+console.log(`Cashback               - ${discount} Taka,`)
+console.log(`Total Payable Amount   - ${discountedPrice} Taka,`)
+console.log(`Home Delevery          - ${discountedPrice+shipping} Taka.`)
---
+++ main.js
@@ -0,0 +1,139 @@
+// const book1 = {
+//     name: "Book11",
+//     writer: "writer2",
+//     price: 0,
+//     rating: 4.5
+// }
+// const book2 = {
+//     name: "Book22",
+//     writer: "writer2",
+//     price: 0,
+//     rating: 4.5
+// }
+// const book3 = {
+//     name: "Book22",
+//     writer: "writer2",
+//     price: 500,
+//     rating: 4.5
+// }
+
+
+// ***************Methode One****************************
+
+
+
+// const cart = [book1 , book2 , book3]
+
+// let totalPrice = 50;
+
+// if(cart.length!=0){
+//     // for(i=0; i<cart.length; i++){
+//     //     totalPrice += cart[i].price;
+//     // }
+
+//     cart.forEach(function(product){
+//     totalPrice += product.price
+// }
+// )
+    
+// }else{
+//     totalPrice=0
+// }
+// console.log(totalPrice);
+
+
+
+// ***************Methode Two*****************************
+
+
+// const cart = [book1 , book2 , book3]
+
+// let totalPrice = 50;
+
+// if(cart.length!=0){
+
+//     cart.forEach(function(product){
+//     totalPrice += product.price
+// }
+// )
+    
+// }else{
+//     totalPrice=0
+// }
+// console.log(totalPrice);
+
+
+
+
+
+// ***************Methode Three*****************************
+
+
+// const cart = [book1 , book2 , book3]
+
+// let totalPrice = 50;
+
+// cart.length != 0 ? 
+
+// cart.forEach(function(product){
+//     totalPrice += product.price
+// }) : totalPrice=0
+
+// console.log(totalPrice);
+
+
+
+
+
+
+// ************************************Task Two******************
+
+
+
+// let shopping = 500;
+// let discountedPrice = shopping - shopping*0.05
+
+// if(shopping>=500){
+
+//     if(shopping>2400){
+//         discountedPrice = shopping - 120
+//     }
+
+
+// }else{
+//     discountedPrice = shopping
+// }
+
+
+// console.log(discountedPrice)
+
+
+
+// ************************************Task one + two******************
+
+
+// const cart = [book1 , book2 , book3]
+
+// let bookPrice = 0;
+
+// const shipping = 50;
+
+// const productPrice = cart.forEach(function(product){
+//         bookPrice += product.price
+//     })
+
+// let discountedPrice = bookPrice - bookPrice*0.05
+
+// if(bookPrice>=500){
+
+//     if(bookPrice>2400){
+//         discountedPrice = bookPrice - 120
+//     }
+
+
+// }else{
+//     discountedPrice = bookPrice
+// }
+
+
+// console.log(discountedPrice+shipping)
